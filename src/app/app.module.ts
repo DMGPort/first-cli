faf3aed1453a9da72fd54e3ed9ad13c2d064f4b5
David Mar Gunnarsson <dmrgunnarsson@gmail.com>
2016-12-07 03:00:52 +0000

    menubar and photogrid components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { BigCarouselComponent } from './big-carousel/big-carousel.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { GridGalleryComponent } from './grid-gallery/grid-gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BigCarouselComponent
+    BigCarouselComponent,
+    MenuBarComponent,
+    GridGalleryComponent
   ],
   imports: [
     BrowserModule,
